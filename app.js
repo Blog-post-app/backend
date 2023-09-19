fab68922ecff688109a9a460afcf98f0315f432f
muhammeddilmacc <muhammeddilmacc@gmail.com>
2023-09-19 12:43:26 +0300

    missing update

--- app.js
+++ app.js
@@ -91,7 +91,9 @@ if (process.env.NODE_ENV === 'production') {
   // app.get('*', (req, res) =>
   //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   // )
-  res.send('API is running....')
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....')
